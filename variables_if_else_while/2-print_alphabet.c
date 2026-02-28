8c35f910a0a9e2dc8023e7185d98eff43443ce07
ReemaAlshahrani <reema.alshahrani.ali@gmail.com>
2026-02-28 19:45:17 +0000

    Final fix for tasks 0 and 2

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,26 +1,19 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 
 /**
- * main - prints the last digit of the random number stored in n
+ * main - prints the alphabet in lowercase
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int n;
-	int last_d;
+	char letter = 'a';
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	last_d = n % 10;
-	printf("Last digit of %d is %d ", n, last_d);
-	if (last_d > 5)
-		printf("and is greater than 5\n");
-	else if (last_d == 0)
-		printf("and is 0\n");
-	else
-		printf("and is less than 6 and not 0\n");
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
 	return (0);
 }
