c939251fc4db7bd9b17bfddd0d0ae3f8a54fa100
ReemaAlshahrani <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>
2026-03-03 22:39:28 +0000

    Fix syntax errors in print_square

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -2,15 +2,13 @@
 
 /**
  * print_square - prints a square, followed by a new line
- * @size: the size of the square
- *
- * Return: void
+ * @size: size of the square
  */
 void print_square(int size)
 {
 	int i, j;
 
-	for (size <= 0)
+	if (size <= 0)
 	{
 		_putchar('\n');
 	}
