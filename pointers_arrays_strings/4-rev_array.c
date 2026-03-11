b7175fb7c991c18e9432e10d572d4d9d9613345d
ReemaAlshahrani <reema.alshahrani.ali@gmail.com>
2026-03-11 20:55:52 +0000

    Add reverse_array function

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -2,22 +2,22 @@
 
 /**
  * reverse_array - Reverses the content of an array of integers.
- * @a: The array of integers.
+ * @a: An array of integers.
  * @n: The number of elements in the array.
  *
- * Return: void.
+ * Return: nothing.
  */
 void reverse_array(int *a, int n)
 {
 	int i;
 	int temp;
 
-	/* Loop only to the middle of the array */
-	for (i = 0; i < n / 2; i++)
+	i = 0;
+	while (i < n / 2)
 	{
-		/* Swap elements using a temporary variable */
 		temp = a[i];
 		a[i] = a[n - 1 - i];
 		a[n - 1 - i] = temp;
+		i++;
 	}
 }
