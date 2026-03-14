051fe8b6dc62f89c01825b703a4a6765f3fd75ea
ReemaAlshahrani <reema.alshahrani.ali@gmail.com>
2026-03-14 18:54:09 +0000

    Add program to multiply two numbers

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - Multiplies two numbers
+ * @argc: The number of arguments passed to the program
+ * @argv: The array of pointers to the arguments
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+
+	/* Check if the correct number of arguments (program name + 2 numbers) is provided */
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	/* Convert the string arguments to integers */
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	/* Multiply the two numbers and print the result */
+	result = num1 * num2;
+	printf("%d\n", result);
+
+	return (0);
+}
