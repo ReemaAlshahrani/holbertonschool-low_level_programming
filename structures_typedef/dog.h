2e3b18948ddfc9a53f22c03c610a9cdc97627450
ReemaAlshahrani <reema.alshahrani.ali@gmail.com>
2026-04-04 16:35:26 +0000

    Define struct dog in dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A new type describing a dog
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+ *
+ * Description: Longer description of the struct dog in the memory
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
