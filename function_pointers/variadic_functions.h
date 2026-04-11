97fca236cc81959c216201eca66fed6b36c365b2
ReemaAlshahrani <reema.alshahrani.ali@gmail.com>
2026-04-11 16:46:42 +0000

    Add sum_them_all function and header file

---
+++ function_pointers/variadic_functions.h
@@ -0,0 +1,17 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+#include <stdarg.h>
+
+/* Prototype for _putchar */
+int _putchar(char c);
+
+/* Prototype for task 0 */
+int sum_them_all(const unsigned int n, ...);
+
+/* Prototypes for future tasks (add them as you progress) */
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif /* VARIADIC_FUNCTIONS_H */
