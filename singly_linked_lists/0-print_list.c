d4e8fcbc05a4a745a0d1c73eb574060ec52cdf73
ReemaAlshahrani <reema.alshahrani.ali@gmail.com>
2026-04-23 16:51:17 +0000

    Final fix for task 0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,9 +1,8 @@
-#include <stdio.h>
 #include "lists.h"
 
 /**
  * print_list - prints all the elements of a list_t list
- * @h: pointer to the start of the list
+ * @h: pointer to the list_t list to print
  *
  * Return: the number of nodes
  */
