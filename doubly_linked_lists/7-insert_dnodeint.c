3052e520ca48612bc448ea976d189a86a86c38d8
ReemaAlshahrani <reema.alshahrani.ali@gmail.com>
2026-04-25 18:19:55 +0000

    Fix Betty line count warning and implement insert_at_index

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -14,29 +14,21 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new, *temp = *h;
 	unsigned int i = 0;
 
-	if (h == NULL)
-		return (NULL);
-
-	/* If inserting at the very beginning (index 0) */
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
-	/* Traverse the list to find the node before the target index */
 	while (temp != NULL && i < (idx - 1))
 	{
 		temp = temp->next;
 		i++;
 	}
 
-	/* If the index is out of bounds */
 	if (temp == NULL)
 		return (NULL);
 
-	/* If the target index is at the very end of the list */
-	if (temp->next == NULL && i == (idx - 1))
+	if (temp->next == NULL)
 		return (add_dnodeint_end(h, n));
 
-	/* Insertion in the middle of the list */
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
@@ -44,12 +36,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new->n = n;
 	new->next = temp->next;
 	new->prev = temp;
-
-	/* Link the subsequent node back to the new node */
-	if (temp->next != NULL)
-		temp->next->prev = new;
-
-	/* Link the current node forward to the new node */
+	temp->next->prev = new;
 	temp->next = new;
 
 	return (new);
