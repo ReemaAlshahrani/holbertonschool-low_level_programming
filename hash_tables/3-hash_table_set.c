af91cdc9042e2fabecd28132316906dfff319c9c
ReemaAlshahrani <reema.alshahrani.ali@gmail.com>
2026-05-09 20:10:45 +0000

    Refactor hash_table_set to meet Betty 40-line limit

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -4,57 +4,48 @@
  * hash_table_set - Adds an element to the hash table.
  * @ht: The hash table to add/update the key/value to.
  * @key: The key (cannot be an empty string).
- * @value: The value associated with the key (must be duplicated).
+ * @value: The value associated with the key.
  *
  * Return: 1 if it succeeded, 0 otherwise.
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index;
-	hash_node_t *new_node, *temp;
-	char *new_value;
+	unsigned long int idx;
+	hash_node_t *new, *tmp;
+	char *val_dup;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
-
-	new_value = strdup(value);
-	if (new_value == NULL)
+	val_dup = strdup(value);
+	if (val_dup == NULL)
 		return (0);
-
-	index = key_index((const unsigned char *)key, ht->size);
-
-	/* 1. Check if key already exists to update its value */
-	temp = ht->array[index];
-	while (temp)
+	idx = key_index((const unsigned char *)key, ht->size);
+	tmp = ht->array[idx];
+	while (tmp)
 	{
-		if (strcmp(temp->key, key) == 0)
+		if (strcmp(tmp->key, key) == 0)
 		{
-			free(temp->value);
-			temp->value = new_value;
+			free(tmp->value);
+			tmp->value = val_dup;
 			return (1);
 		}
-		temp = temp->next;
+		tmp = tmp->next;
 	}
-
-	/* 2. Key doesn't exist, create a new node */
-	new_node = malloc(sizeof(hash_node_t));
-	if (new_node == NULL)
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
 	{
-		free(new_value);
+		free(val_dup);
 		return (0);
 	}
-
-	new_node->key = strdup(key);
-	if (new_node->key == NULL)
+	new->key = strdup(key);
+	if (new->key == NULL)
 	{
-		free(new_value);
-		free(new_node);
+		free(val_dup);
+		free(new);
 		return (0);
 	}
-
-	new_node->value = new_value;
-	new_node->next = ht->array[index]; /* Add at the beginning of the list */
-	ht->array[index] = new_node;
-
+	new->value = val_dup;
+	new->next = ht->array[idx];
+	ht->array[idx] = new;
 	return (1);
 }
